f75f7f135af123f5dc97e07cfcaf053f359feacb
vizik24 <max.mycroft@gmail.com>
2025-01-10 16:37:26 +0000

    reduced text size on bottom CTA heading

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,17 +1,18 @@
 import CtaButton from "./CtaButton";
 
 export default function CTA() {
-  return (
-    <div className="m-20">
-      <p className="text-5xl font-bold">Acheive more with Goals<span className="text-primary">Tree</span></p>
-      <br></br>
-      <p className="text-xl">
-        Ready to turn your goals into reality? GoalsTree is here to help you
-        break down your ambitions, track your progress, and celebrate every win
-        along the way. Get started for free today.
-      </p>
-      <br></br>
-      <CtaButton></CtaButton>
+return (
+    <div className="m-20 text-center">
+        <p className="text-4xl font-bold">Acheive more with Goals<span className="text-primary">Tree</span>
+        </p>
+        <br></br>
+        <p className="text-xl">
+            Ready to turn your goals into reality? GoalsTree is here to help you
+            break down your ambitions, track your progress, and celebrate every win
+            along the way. Get started for free today.
+        </p>
+        <br></br>
+        <CtaButton></CtaButton>
     </div>
-  );
+);
 }
