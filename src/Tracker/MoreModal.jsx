01ecd3bf34ae99b75436ffec909c7338171ba738
vizik24 <max.mycroft@gmail.com>
2025-02-13 17:32:49 +0000

    added conditions for rendering information in moremodal

--- src/Tracker/MoreModal.jsx
+++ src/Tracker/MoreModal.jsx
@@ -17,6 +17,7 @@
  * @returns {JSX.Element} The rendered MotivationModal component.
  */
 
+import { useEffect, useState } from "react";
 import DeleteButton from "./DeleteButton";
 import DeleteModal from "./DeleteModal";
 import EditButton from "./EditButton"
@@ -24,16 +25,27 @@ import EditModal from "./EditModal";
 
 export default function MoreModal({setGoals, goals, index }) {
 
-  console.log('more modal goal index', index)
+  const [title, setTitle] = useState(goals[index] ? goals[index].title : '')
+  const [description, setDescription] = useState(goals[index] ? goals[index].description : '')
+  const [motivation, setMotivation] = useState(goals[index] ? goals[index].motivation : 'Edit goal to add a motivation.')
+  const [goalIndex, setGoalIndex] = useState(goals.index ? index : null)
+
+  // re render title, description, motivation if goals state changes
+  useEffect( () => {
+    setTitle(goals[index] ? goals[index].title : '')
+    setDescription(goals[index] ? goals[index].description : '')
+    setMotivation(goals[index] ? goals[index].motivation : 'Edit goal to add a motivation.')
+    setGoalIndex(goals[index] ? index : null)
+  }, goals.length)
 
 
   return (
     <>
     {/* add edit modals to the dom so we can display them when eddit button is pressed. */}
-    <EditModal goals={goals} index={index} setGoals={setGoals}></EditModal>
+    <EditModal goals={goals} index={goalIndex} setGoals={setGoals}></EditModal>
 
     {/* add delete modal to the dom so we can display them when edit button is pressed */}
-    <DeleteModal setGoals={setGoals} index={index}></DeleteModal>
+    <DeleteModal setGoals={setGoals} index={goalIndex}></DeleteModal>
 
     <dialog
       id={`${index}-more-modal`}
@@ -44,15 +56,15 @@ export default function MoreModal({setGoals, goals, index }) {
           <div className="flex justify-start">
           <DeleteButton index={index} setGoals={setGoals} />
           </div>
-          <h3 className="font-bold text-lg">{goals[index].title}</h3>
+          <h3 className="font-bold text-lg">{title}</h3>
           <div className="flex justify-end">
           <EditButton index={index} setGoals={setGoals} goals={goals} />
           </div>
 
         </div>
-        <p className="py-2 text-sm text-neutral-500">{goals[index].description}</p>
+        <p className="py-2 text-sm text-neutral-500">{description}</p>
         <p className="py-2 text-m">Motivation:</p>
-        <p className="py-2 text-sm text-neutral-500">{ goals[index].motivation ? goals[index].motivation : 'Edit goal to add a motivation.'}</p>
+        <p className="py-2 text-sm text-neutral-500">{motivation}</p>
         <div className="modal-action">
           <form method="dialog">
             {/* if there is a button in form, it will close the modal */}
